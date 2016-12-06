08499a3be8f953208f605a91a1c25a8d4eef4494
Andrew Moskwoitz <andrewmoskowitz@gmail.com>
2016-12-05 21:13:42 -0500

    Created table view, cell, + cell view controller

---
+++ Project 2/Project 2/MainTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainTableViewCell.swift
+//  Project 2
+//
+//  Created by Andrew Moskowitz on 12/5/16.
+//  Copyright © 2016 Andrew Moskowitz. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
